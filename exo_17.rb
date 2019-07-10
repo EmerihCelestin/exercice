424ae1fd12de5db4b743141626bdaa583f6859ba
emerihcelestin <emerih02@gmail.com>
2019-07-10 16:34:40 +0300

    mise a jour

--- exo_17.rb
+++ exo_17.rb
@@ -4,12 +4,13 @@ old = gets.chomp.to_i
 years = 2019
 user_years = years - old
 ans = 0
-while user_years <= years
+while user_years < years
+	
 	 if (old==ans)
 	 	puts "Il y a" + " " + "#{ans}" + " " + "ans, tu avais la moitié de l'âge que tu as aujourd'hui."
-	 	 
-	 	 end
-	puts "Il y a" + " " + "#{ans}" + " " + "ans, tu avais " + " " + "#{old}" + "ans." 
+	 else
+	 	puts "Il y a" + " " + "#{ans}" + " " + "ans, tu avais " + " " + "#{old}" + "ans." 
+	 end
    	years -= 1
 	old -= 1
 	ans +=1
