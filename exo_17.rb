493609c59bfb407e9c75647fd25126589f875b24
emerihcelestin <emerih02@gmail.com>
2019-07-10 16:01:43 +0300

    exo_17remplacé

--- exo_17.rb
+++ exo_17.rb
@@ -5,10 +5,12 @@ years = 2019
 user_years = years - old
 ans = 0
 while user_years <= years
-
+	 if (old==ans)
+	 	puts "Il y a" + " " + "#{ans}" + " " + "ans, tu avais la moitié de l'âge que tu as aujourd'hui."
+	 	 
+	 	 end
 	puts "Il y a" + " " + "#{ans}" + " " + "ans, tu avais " + " " + "#{old}" + "ans." 
    	years -= 1
 	old -= 1
 	ans +=1
-
 end
